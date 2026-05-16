cbb31d36f2750a8c1dcb7063ed5cf693ad69b6c6
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-05-16 01:48:01 -0500

    Remove both generate-build scripts on x32 and Linux versions
    They'll need to be either regenerated for X2R, or not be included at all

--- data/PGEx32/generate-build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Copying data...
-xcopy /Y /I /E /K /H ..\PGE\configs configs
-xcopy /Y /I /E /K /H ..\PGE\tools tools
-copy ..\PGE\update-pge* .
-echo Patching scripts...
-tools\fart update-pge* "moondust-win64/pge-project-master-win64.zip" "moondust-win32/pge-project-master-win32.zip"
-tools\fart update-pge* "PGE_IS_COPY=false" "PGE_IS_COPY=true"
-call update-pge.cmd %1
