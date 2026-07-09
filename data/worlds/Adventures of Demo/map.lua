cae906a00436b2038048030158a67a22e34fd62b
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-07-08 23:58:38 -0500

    Add "Adventures of Demo" to the default episode list

---
+++ data/worlds/Adventures of Demo/map.lua
@@ -0,0 +1,3 @@
+-- Only allow the first 5 characters
+local playerManager = require("playermanager")
+playerManager.overworldCharacters = {CHARACTER_MARIO, CHARACTER_LUIGI, CHARACTER_PEACH, CHARACTER_TOAD, CHARACTER_LINK}
